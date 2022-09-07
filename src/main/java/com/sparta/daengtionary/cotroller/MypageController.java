3a4c4927bb42fad39194d28b7ce381e87760173c
Jinu0729 <jinu0729@gmail.com>
2022-09-07 17:10:41 +0900

    [feat] 반려견 수정기능 추가 #68 마이페이지 반려견 수정기능 추가

--- src/main/java/com/sparta/daengtionary/cotroller/MypageController.java
+++ src/main/java/com/sparta/daengtionary/cotroller/MypageController.java
@@ -14,26 +14,42 @@ import org.springframework.web.multipart.MultipartFile;
 public class MypageController {
     private final MypageService mypageService;
 
-    @PatchMapping("/nick")
-    public ResponseEntity<?> updateByNick(@RequestBody MemberRequestDto.Update update) {
-        return mypageService.updateByNick(update);
-    }
-
-    @PostMapping("/dog")
-    public ResponseEntity<?> createDogProfile(@RequestPart(value = "data") DogRequestDto requestDto,
-                                              @RequestPart(value = "image", required = false) MultipartFile multipartFile) {
-        return mypageService.createDogProfile(requestDto, multipartFile);
-    }
-
     @GetMapping("/info")
     public ResponseEntity<?> getMemberInfo() {
         return mypageService.getMemberInfo();
     }
 
-    @PatchMapping("/dog/{dogNo}")
-    public ResponseEntity<?> updateByDog(@PathVariable Long dogNo,
-                                         @RequestBody DogRequestDto requestDto) {
+    @PatchMapping("/nick")
+    public ResponseEntity<?> updateByNick(@RequestBody MemberRequestDto.Update update) {
+        return mypageService.updateByNick(update);
+    }
 
-        return mypageService.updateByDog(dogNo, requestDto);
+    @PostMapping("/dogs")
+    public ResponseEntity<?> createDogProfile(@RequestPart(value = "data") DogRequestDto requestDto,
+                                              @RequestPart(value = "image", required = false) MultipartFile multipartFile) {
+        return mypageService.createDogProfile(requestDto, multipartFile);
+    }
+
+    @PatchMapping("/dogs/{dogNo}")
+    public ResponseEntity<?> updateByDogProfile(@PathVariable Long dogNo,
+                                                @RequestPart(value = "data") DogRequestDto requestDto,
+                                                @RequestPart(value = "image", required = false) MultipartFile multipartFile) {
+        return mypageService.updateByDogProfile(dogNo, requestDto, multipartFile);
+    }
+
+    @PatchMapping("/dogs/image/{dogNo}")
+    public ResponseEntity<?> updateByDogImage(@PathVariable Long dogNo,
+                                              @RequestPart MultipartFile multipartFile) {
+        return mypageService.updateByDogImage(dogNo, multipartFile);
+    }
+
+    @DeleteMapping("/dogs/{dogNo}")
+    public ResponseEntity<?> deleteByDogProfile(@PathVariable Long dogNo) {
+        return mypageService.deleteByDogProfile(dogNo);
+    }
+
+    @DeleteMapping("/dogs/image/{dogNo}")
+    public ResponseEntity<?> deleteByDogImage(@PathVariable Long dogNo) {
+        return mypageService.deleteByDogImage(dogNo);
     }
 }
