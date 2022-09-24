8b33158b9432822b4e72aeca24663dfc20f1370f
zemiles <abeb3@naver.com>
2022-09-24 14:16:28 +0900

    [feat] 메인 페이지 생성 #179
    메인 페이지 생성

---
+++ src/main/java/com/sparta/daengtionary/aop/server/controller.java
@@ -0,0 +1,13 @@
+package com.sparta.daengtionary.aop.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+
+    @GetMapping("/")
+    public String Home(){
+        return "Home";
+    }
+}
