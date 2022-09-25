9328c23fe5dc31bdbf6d80bb8cd44114ead667e1
zemiles <abeb3@naver.com>
2022-09-25 13:37:40 +0900

    [feat] 무중단 배포 / ci/cd 생성 #139
    최종 테스트 4

--- src/main/java/com/sparta/daengtionary/aop/server/controller.java
+++ src/main/java/com/sparta/daengtionary/aop/server/controller.java
@@ -3,11 +3,13 @@ package com.sparta.daengtionary.aop.server;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 public class controller {
 
     @GetMapping("/")
     public String Home(){
-        return "HomeTest";
+        return "HomeTest " + LocalDate.now();
     }
 }
