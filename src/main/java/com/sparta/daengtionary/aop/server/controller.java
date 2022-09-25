240757cd090c0a7a0ca2cf4259ff8a5d1db331e2
zemiles <abeb3@naver.com>
2022-09-25 15:47:19 +0900

    [feat] 무중단 배포 / ci/cd 생성 #139
    배포 테스트

--- src/main/java/com/sparta/daengtionary/aop/server/controller.java
+++ src/main/java/com/sparta/daengtionary/aop/server/controller.java
@@ -1,15 +1,16 @@
 package com.sparta.daengtionary.aop.server;
 
+import org.joda.time.LocalTime;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
 
 @RestController
 public class controller {
+    private final LocalTime now = new LocalTime();
 
     @GetMapping("/")
-    public String Home(){
-        return "HomeTest " + LocalDate.now();
+    public String Home() {
+        return "HomeTest " + now;
     }
 }
