3067d0220aee93621d75a317c4615c843da1a0a6
zemiles <abeb3@naver.com>
2022-09-27 16:46:38 +0900

    [feat] member 이미지 추가 #135
    member 이미지 추가

--- src/main/java/com/sparta/daengtionary/aop/supportrepository/PostDetailRepositorySupport.java
+++ src/main/java/com/sparta/daengtionary/aop/supportrepository/PostDetailRepositorySupport.java
@@ -1,8 +1,12 @@
 package com.sparta.daengtionary.aop.supportrepository;
 
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sparta.daengtionary.category.recommend.domain.Map;
+import com.sparta.daengtionary.category.recommend.dto.response.MapDetailResponseDto;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.springframework.stereotype.Repository;
 
@@ -26,21 +30,31 @@ public class PostDetailRepositorySupport extends QuerydslRepositorySupport {
     }
 
 
-    public List<Tuple> findByMapDetail(Long mapNo) {
+    public List<MapDetailResponseDto> findByMapDetail(Long mapNo) {
         return queryFactory
-                .select(
-                        map,
-                        member,
-                        mapImg,
-                        mapInfo1,
-                        mapReview
-                )
+                .select(Projections.fields(
+                        MapDetailResponseDto.class,
+                        map.mapNo,
+                        map.member.nick,
+                        map.title,
+                        map.address,
+                        map.category,
+                        map.content,
+                        map.star.as("mapStar"),
+                        map.view,
+                        map.createdAt,
+                        map.modifiedAt,
+                        mapImg.mapImgUrl.as("mapImgUrl")
+                ))
                 .from(map)
-                .join(map.member,member)
-                .join(map.mapImgs,mapImg)
-                .join(map.mapInfos,mapInfo1)
-                .join(map.mapReviews,mapReview)
-                .where(map.mapNo.eq(mapNo))
+                .join(map.member, member)
+                .join(mapImg.map,map)
+//                .on(map.mapNo.eq(mapImg.map.mapNo))
+//                .leftJoin(mapInfo1)
+//                .on(map.mapNo.eq(mapInfo1.map.mapNo))
+//                .leftJoin(mapReview)
+//                .on(map.mapNo.eq(mapReview.map.mapNo))
+//                .where(map.mapNo.eq(mapNo))
                 .fetch();
     }
 }
