6b2b8cc4ab59d4470ac3d9d444a4e535a4b0d8b0
zemiles <abeb3@naver.com>
2022-09-29 18:09:33 +0900

    [fix] api 오류 수정
    api 오류 수정

--- src/main/java/com/sparta/daengtionary/category/trade/dto/response/TradeDetailResponseDto.java
+++ src/main/java/com/sparta/daengtionary/category/trade/dto/response/TradeDetailResponseDto.java
@@ -1,7 +1,6 @@
 package com.sparta.daengtionary.category.trade.dto.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.sparta.daengtionary.category.recommend.dto.response.ReviewResponseDto;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,33 +21,29 @@ public class TradeDetailResponseDto {
     private String content;
     private int price;
     private int view;
-    private Long reviewCount;
     private Long wishCount;
     private List<String> tradeImgUrl;
-    private List<ReviewResponseDto> reviewList;
     @JsonFormat(pattern = "yyyy년 MM월 dd일 E요일 a hh:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdAt;
     @JsonFormat(pattern = "yyyy년 MM월 dd일 E요일 a hh:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime modifiedAt;
 
     @Builder
-    public TradeDetailResponseDto(Long tradeNo, String nick, String title, Long wishCount, Long reviewCount,
+    public TradeDetailResponseDto(Long tradeNo, String nick, String title, Long wishCount,
                                   String address, String stuffStatus, String exchange, String postStatus,
                                   String content, int price, int view, List<String> tradeImgUrl,
-                                  List<ReviewResponseDto> reviewList, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+                                  LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.tradeNo = tradeNo;
         this.nick = nick;
         this.title = title;
         this.content = content;
         this.price = price;
         this.wishCount = wishCount;
-        this.reviewCount = reviewCount;
         this.address = address;
         this.exchange = exchange;
         this.stuffStatus = stuffStatus;
         this.postStatus = postStatus;
         this.view = view;
-        this.reviewList = reviewList;
         this.tradeImgUrl = tradeImgUrl;
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
