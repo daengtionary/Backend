266ca47bf6a28e639bf5d3e3243506e790da9c7e
zemiles <abeb3@naver.com>
2022-10-02 22:27:57 +0900

    [chore] 빌드 업무 수정 #264
    빌드 업무 수정

--- scripts/run_new_was.sh
+++ scripts/run_new_was.sh
@@ -24,8 +24,9 @@ REPOSITORY=/home/ubuntu/daengtionary
 # shellcheck disable=SC2164
 cd $REPOSITORY
 
-
-JAR_PATH=$REPOSITORY/build/libs/app.jar
+# shellcheck disable=SC2010
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'app.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 nohup java -jar -Dserver.port=${TARGET_PORT} ${JAR_PATH} >/home/ubuntu/daengtionary/nohup.out 2>&1 &
 echo "> Now new WAS runs at ${TARGET_PORT}."
