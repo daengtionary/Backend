317eda7ac06294c5af284710272a3960dc984476
zemiles <abeb3@naver.com>
2022-10-03 13:02:56 +0900

    [chore] 빌드 업무 수정 #264
    빌드 업무 수정

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -6,28 +6,26 @@ TARGET_PORT=0
 
 # Toggle port Number
 if [ ${CURRENT_PORT} -eq 8090 ]; then
-    TARGET_PORT=8091
+  TARGET_PORT=8091
 elif [ ${CURRENT_PORT} -eq 8091 ]; then
-    TARGET_PORT=8090
+  TARGET_PORT=8090
 else
-    echo "> No WAS is connected to nginx"
-    exit 1
+  echo "> No WAS is connected to nginx"
+  exit 1
 fi
-
 
 echo "> Start health check of WAS at 'http://127.0.0.1:${TARGET_PORT}' ..."
 
-for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10
-do
-    echo "> #${RETRY_COUNT} trying..."
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}"  http://127.0.0.1:${TARGET_PORT}/)
+for RETRY_COUNT in 1 2 3 4 5 6 7 8 9 10; do
+  echo "> #${RETRY_COUNT} trying..."
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://127.0.0.1:${TARGET_PORT}/)
 
-    if [ ${RESPONSE_CODE} -eq 200 ]; then
-        echo "> New WAS successfully running"
-        exit 0
-    elif [ ${RETRY_COUNT} -eq 10 ]; then
-        echo "> Health check failed."
-        exit 1
-    fi
-    sleep 10
+  if [ ${RESPONSE_CODE} -eq 200 ]; then
+    echo "> New WAS successfully running"
+    exit 0
+  elif [ ${RETRY_COUNT} -eq 10 ]; then
+    echo "> Health check failed."
+    exit 1
+  fi
+  sleep 10
 done
